b7b101f408b80e113533e4f669cd62d0fde0a164
brwngda <grz.barwinski@gmail.com>
2022-11-26 12:42:48 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,12 +1,34 @@
 package pl.sda.hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 import static pl.sda.hibernate.demo.HibernateUtil.*;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = INSTANCE.getSessionFactory().openSession();
-        session.close();
+// wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            // Tworzymy studenta
+            Student student = new Student();
+            student.setImie("Paweł");
+            student.setDataUrodzenia(LocalDate.of(1990,1,3));
+            student.setKierunekNauczania("Informatyka");
+            student.setIndeks("123123");
+
+            //zapisujemy studenta
+            session.persist(student);
+
+            // zatwierdzamy transakcję
+            transaction.commit();
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
     }
 }
