0d23e8a6d3b10622601eaa9572a1838ae7be8383
brwngda <grz.barwinski@gmail.com>
2022-11-26 14:10:56 +0100

    Usuwanie rekordu po ID

---
+++ src/main/java/pl/sda/hibernate/demo/MainDelete_Delete.java
@@ -0,0 +1,27 @@
+package pl.sda.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.time.LocalDate;
+
+public class MainDelete_Delete {
+    public static void main(String[] args) {
+// wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            // Najpierw SELECT * FROM Student WHERE id = @L
+            Student student = session.get(Student.class, 2L);
+
+            // jeśli udało się znaleźć
+            if (student != null) {
+                // DELETE FROM Student WHERE id = 2L
+                session.remove(student);
+            }
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
+    }
+}
