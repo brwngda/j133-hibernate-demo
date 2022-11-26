d2ce49108d33b20c12a021d784ff77864aadcde2
brwngda <grz.barwinski@gmail.com>
2022-11-26 11:37:49 +0100

    Baza skonfigurowana

--- src/main/java/pl/sda/hibernate/demo/Main.java
+++ src/main/java/pl/sda/hibernate/demo/Main.java
@@ -1,7 +1,12 @@
 package pl.sda.hibernate.demo;
 
+import org.hibernate.Session;
+
+import static pl.sda.hibernate.demo.HibernateUtil.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!~);
+        Session session = INSTANCE.getSessionFactory().openSession();
+        session.close();
     }
 }
